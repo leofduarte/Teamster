d7e1a7f9b431994283468e0ae16d2c47d114ff2e
leofduarte <leandroduarte@ua.pt>
2024-07-03 17:02:02 +0100

    add changes before hosting

--- resources/js/Pages/Invitation.jsx
+++ resources/js/Pages/Invitation.jsx
@@ -1,32 +1,32 @@
-import React from 'react';
-import Logo from '../components/Logo';
-import Events from '../components/Events';
-import Notifications from '../components/Notifications';
-import Invite from '../components/Invite';
-import { Button } from "@/components/ui/button";
-import { Link } from 'react-router-dom';
-
-function Invitation() {
-  return (
-    <div className="min-h-screen bg-[#F8F7FC] flex flex-col items-center p-8 font-poppins">
-      <div className="w-full max-w-5xl grid grid-cols-1 md:grid-cols-3 gap-12">
-        <div className="col-span-1 flex flex-col space-y-8">
-          <Logo />
-          <Events />
-          <Notifications />
-          <div className="flex justify-end">
-            {/*<Link to="/">*/}
-              <Button variant="" className="ml-auto">Voltar</Button>
-            {/*</Link>*/}
-          </div>
-        </div>
-
-        <div className="col-span-2 flex flex-col items-center justify-center">
-          <Invite />
-        </div>
-      </div>
-    </div>
-  );
-}
-
-export default Invitation;
+// import React from 'react';
+// import Logo from '../components/Logo';
+// import Events from '../components/Events';
+// import Notifications from '../components/Notifications';
+// import Invite from '../components/Invite';
+// import { Button } from "@/components/ui/button";
+// import { Link } from 'react-router-dom';
+//
+// function Invitation() {
+//   return (
+//     <div className="min-h-screen bg-[#F8F7FC] flex flex-col items-center p-8 font-poppins">
+//       <div className="w-full max-w-5xl grid grid-cols-1 md:grid-cols-3 gap-12">
+//         <div className="col-span-1 flex flex-col space-y-8">
+//           <Logo />
+//           <Events />
+//           <Notifications />
+//           <div className="flex justify-end">
+//             {/*<Link to="/">*/}
+//               <Button variant="" className="ml-auto">Voltar</Button>
+//             {/*</Link>*/}
+//           </div>
+//         </div>
+//
+//         <div className="col-span-2 flex flex-col items-center justify-center">
+//           <Invite />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+//
+// export default Invitation;
